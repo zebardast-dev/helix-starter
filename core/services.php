225f670f055a818ada80303153d5d0fe40cb2030
Mostafa Zebardast <z.mostafa3637@gmail.com>
2026-06-26 18:01:43 +0330

    Add SVG upload support and remove legacy SvgUpload service
    - functions.php: add section 4 for media uploads with SvgHandler
    - core/services.php: remove SvgUpload (replaced by FileUploader system)

--- core/services.php
+++ core/services.php
@@ -4,12 +4,10 @@ use Helix\Theme\Setup;
 use Helix\Theme\Assets;
 use Helix\Theme\Sidebars;
 use Helix\Support\Cleanup;
-use Helix\Support\SvgUpload;
 
 return [
     Setup::class,
     Assets::class,
     Sidebars::class,
     Cleanup::class,
-    SvgUpload::class,
 ];
